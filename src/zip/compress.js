1c4f6b8b3c41b9991b739fba04c3be84a2c44a5f
destolyar <dead.stolyar@gmail.com>
2022-12-03 00:37:25 +0400

    zip section

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,12 @@
+import zlib from 'zlib'
+import fs from 'fs'
+
 const compress = async () => {
-    // Write your code here 
+    const gzip = zlib.createDeflate()
+    const inputStream = fs.createReadStream('./files/fileToCompress.txt', "utf-8")
+    const outputStream = fs.createWriteStream('./files/archive.gz')
+
+    inputStream.pipe(gzip).pipe(outputStream)
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,12 @@
+import zlib from 'zlib'
+import fs from 'fs'
+
 const decompress = async () => {
-    // Write your code here 
+    const gzip = zlib.createInflate()
+    const inputStream = fs.createReadStream('./files/archive.gz')
+    const outputStream = fs.createWriteStream('./files/fileToCompress.txt')
+
+    inputStream.pipe(gzip).pipe(outputStream)
 };
 
 await decompress();
