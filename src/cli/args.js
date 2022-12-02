0eccc0367e05f61f783fc75309a4329c87f844ed
destolyar <dead.stolyar@gmail.com>
2022-12-02 15:56:20 +0400

    feat: cli section

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+    const RssVariables = process.argv.map((variable, index) => {
+        if (variable.includes("--")) return `${variable.slice(2)} is ${process.argv[index + 1]}`
+    }).filter(variable => !!variable)
+
+    const output = RssVariables.reduce((output, element, index) => (index === RssVariables.length - 1) ? output += element : output += element + ", ", "");
+    console.log(output)
 };
 
 parseArgs();
